25a16a5025343e962a81f1503091d68fa6f278a9
BrightAvocado <arth.gassner@gmail.com>
2018-10-11 09:32:17 +0200

    Added State.java and wrote the skeleton for it

---
+++ src/State.java
@@ -0,0 +1,24 @@
+import logist.topology.Topology.City;
+
+/**
+ * This class represents a State for the deliberative solution. We need this
+ * because last time, debugging was a pain.
+ */
+public class State {
+
+	public State() {
+		//TODO
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		//TODO
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		//TODO
+		return "todo";
+	}
+}
