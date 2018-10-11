36f66ea192bde33330d059ccdd0c9ab605a73668
BrightAvocado <arth.gassner@gmail.com>
2018-10-11 14:50:34 +0200

    Implement equals(Object)

--- src/State.java
+++ src/State.java
@@ -6,27 +6,27 @@ import logist.topology.Topology.City;
  * This class represents a State for the deliberative solution. We need this
  * because last time, debugging was a pain.
  * 
- * The state of the vehicle can be entirely defined by where it's coming from and where it's going. (really ? No tasks it's carrying ?)
+ * The state of the vehicle can be entirely defined by where it's coming from
+ * and where it's going. (really ? No tasks it's carrying ?)
  */
 public class State {
 
-	private City fromCity;
+	private City currentCity;
 	private ArrayList<City> possibleCities;
-	
-	public State(City fromCity, ArrayList<City> possibleCities) {
-		this.fromCity = fromCity;
+
+	public State(City currentCity, ArrayList<City> possibleCities) {
+		this.currentCity = currentCity;
 		this.possibleCities = possibleCities;
 	}
 
 	@Override
 	public boolean equals(Object that) {
 		//TODO
-		return true;
+		return false;
 	}
-	
+
 	@Override
 	public String toString() {
-		//TODO
-		return "todo";
+		return currentCity + " -> " + possibleCities;
 	}
 }
