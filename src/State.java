270443b1b524274d4ccd02cf2458a9d65c081978
BrightAvocado <arth.gassner@gmail.com>
2018-10-11 12:54:50 +0200

    Drafted definition of state

--- src/State.java
+++ src/State.java
@@ -3,11 +3,17 @@ import logist.topology.Topology.City;
 /**
  * This class represents a State for the deliberative solution. We need this
  * because last time, debugging was a pain.
+ * 
+ * The state of the vehicle can be entirely defined by where it's coming from and where it's going. (really ? No tasks it's carrying ?)
  */
 public class State {
 
-	public State() {
-		//TODO
+	private City fromCity;
+	private City toCity;
+	
+	public State(City fromCity, City toCity) {
+		this.fromCity = fromCity;
+		this.toCity = toCity;
 	}
 
 	@Override
